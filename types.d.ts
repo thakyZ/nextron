1e0a43af92ce26ab50eb4431f927644f7f5cd47d
thakyZ <950594+thakyZ@users.noreply.github.com>
2025-08-02 11:55:25 -0700

    chore: add types for the nextron configuration

---
+++ types.d.ts
@@ -0,0 +1,31 @@
+declare module 'nextron';
+
+import type { Configuration } from "webpack";
+
+/**
+ * The configuration interface for nextron.
+ */
+export declare interface NextronConfig {
+  /**
+   * Specify an alternate main src directory, defaults to 'main'.
+   */
+  rendererSrcDir?: string;
+
+  /**
+   * Specify an alternate renderer src directory, defaults to 'renderer'.
+   */
+  mainSrcDir?: string;
+
+  /**
+   * Specify a startup delay in milliseconds to retry to test if the debugger port is open, defaults to '10000'.
+   */
+  startupDelay?: number;
+
+  /**
+   * Override for the main process's webpack
+   * @param {import('webpack').Configuration} config An instance of the base webpack configuration.
+   * @param {'development'|'production'} mode A string specifying the build mode of the program.
+   * @returns {import('webpack').Configuration} Returns an instance of the final configuration instance.
+   */
+  webpack?: (config: Configuration, mode: 'development' | 'production') => Configuration;
+}
